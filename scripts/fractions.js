42366b047e08d79bb12c5be36b94b4bb5eec84da
Alex Bokii <vanilinka33@gmail.com>
2015-05-09 18:49:52 +0100

    Added function that subtracts fractions

---
+++ scripts/fractions.js
@@ -0,0 +1,105 @@
+$(function() {
+
+    function obtainFraction(num, den) {
+        var result = [];
+        for(var i = 0; i < num; i++) {
+            if(num % i == 0 && den % i == 0) {
+                result.unshift(num / i);
+                result.unshift(den / i);
+            }
+        }
+        return result;
+    }
+
+    function addNumeratorsAndShowResult(num1, num2, den) {
+        var numerator = num1 + num2;
+        var obtainedFraction = obtainFraction(numerator, den);
+        if(obtainedFraction.length > 0) {
+            numerator = obtainedFraction[1];
+            var denominator = obtainedFraction[0];
+            $('#fractionAddition .resultNumerator').val(numerator);
+            $('#fractionAddition .resultDenominator').val(denominator);
+        }
+        else {
+            $('#fractionAddition .resultNumerator').val(numerator);
+            $('#fractionAddition .resultDenominator').val(den);
+        }
+    }
+
+    function subtractNumeratorsAndShowResult(num1, num2, den) {
+        var numerator = num1 - num2;
+        var obtainedFraction = obtainFraction(numerator, den);
+        if(obtainedFraction.length > 0) {
+            numerator = obtainedFraction[1];
+            var denominator = obtainedFraction[0];
+            $('#fractionSubtraction .resultNumerator').val(numerator);
+            $('#fractionSubtraction .resultDenominator').val(denominator);
+        }
+        else {
+            $('#fractionSubtraction .resultNumerator').val(numerator);
+            $('#fractionSubtraction .resultDenominator').val(den);
+        }
+    }
+
+    function findCommonDenominatorAndChangeNumeratorsIfNeed(firstNumerator, secondNumerator, firstDenominator, secondDenominator) {
+        var fraction = {
+            firstNumerator: firstNumerator,
+            secondNumerator: secondNumerator,
+            firstDenominator: firstDenominator,
+            secondDenominator: secondDenominator,
+            denominator: undefined
+        }
+
+        if(firstDenominator != secondDenominator) {
+            if(firstDenominator % secondDenominator == 0) {
+                fraction.secondNumerator = firstDenominator / secondDenominator * secondNumerator;
+                fraction.denominator = firstDenominator;
+            }
+            else if (secondDenominator % firstDenominator == 0){
+                fraction.firstNumerator = secondDenominator / firstDenominator * firstNumerator;
+                fraction.denominator = secondDenominator;
+            }
+            else {
+                fraction.denominator = firstDenominator * secondDenominator;
+                fraction.firstNumerator = firstNumerator * secondDenominator;
+                fraction.secondNumerator = secondNumerator * firstDenominator;
+            } 
+        }
+        else {
+            fraction.denominator = firstDenominator;
+        }
+        return fraction;
+    }
+
+    // fraction(addition)
+    $('#countFrAd').on('click', function(e) {
+        e.preventDefault();
+        var firstNumerator = parseInt($('#fractionAddition .firstNumerator').val());
+        var secondNumerator = parseInt($('#fractionAddition .secondNumerator').val());
+        var firstDenominator = parseInt($('#fractionAddition .firstDenominator').val());
+        var secondDenominator = parseInt($('#fractionAddition .secondDenominator').val());
+
+        var resultFraction = findCommonDenominatorAndChangeNumeratorsIfNeed(firstNumerator, secondNumerator, firstDenominator, secondDenominator);
+        addNumeratorsAndShowResult(resultFraction.firstNumerator, resultFraction.secondNumerator, resultFraction.denominator);
+    });
+
+    // fraction(substraction)
+    $('#countFrSub').on('click', function(e) {
+        e.preventDefault();
+        var firstNumerator = parseInt($('#fractionSubtraction .firstNumerator').val());
+        var secondNumerator = parseInt($('#fractionSubtraction .secondNumerator').val());
+        var firstDenominator = parseInt($('#fractionSubtraction .firstDenominator').val());
+        var secondDenominator = parseInt($('#fractionSubtraction .secondDenominator').val());
+
+        var resultFraction = findCommonDenominatorAndChangeNumeratorsIfNeed(firstNumerator, secondNumerator, firstDenominator, secondDenominator);
+        subtractNumeratorsAndShowResult(resultFraction.firstNumerator, resultFraction.secondNumerator, resultFraction.denominator);
+    });
+
+    $('.clearForm').on('click', function(e) {
+        e.preventDefault();
+        $('form input').val("");
+    });
+
+    
+
+});
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,41 +18,4 @@ $(function() {
             $('#isPrime output').html("<p class='bg-success'>Number is prime!</p>");
         }  
     });
-
-    // fraction(addition)
-    $('#countFrAd').on('click', function(e) {
-        e.preventDefault();
-        // get all elements of fraction
-        var firstNumerator = parseInt($('#fractionAddition .firstNumerator').val());
-        var secondNumerator = parseInt($('#fractionAddition .secondNumerator').val());
-        var firstDenominator = parseInt($('#fractionAddition .firstDenominator').val());
-        var secondDenominator = parseInt($('#fractionAddition .secondDenominator').val());
-
-        if(firstDenominator != secondDenominator) {
-            // find common denominator
-            if(firstDenominator % secondDenominator == 0) {
-                var newSecondNumerator = firstDenominator / secondDenominator * secondNumerator;
-                addNumeratorsAndShowResult(firstNumerator, newSecondNumerator, firstDenominator);
-            }
-            else if (secondDenominator % firstDenominator == 0){
-                var newFirstNumerator = secondDenominator / firstDenominator * firstNumerator;
-                addNumeratorsAndShowResult(newFirstNumerator, secondNumerator, secondDenominator);
-            }
-            else {
-                var newDenominator = firstDenominator * secondDenominator;
-                var newFirstNumerator = firstNumerator * secondDenominator;
-                var newSecondNumerator = secondNumerator * firstDenominator;
-                addNumeratorsAndShowResult(newFirstNumerator, newSecondNumerator, newDenominator);
-            } 
-        }
-        else {
-            addNumeratorsAndShowResult(firstNumerator, secondNumerator, firstDenominator);
-        }
-
-    });
-
-    function addNumeratorsAndShowResult(num1, num2, den) {
-        $('#fractionAddition .resultNumerator').val(num1 + num2);
-        $('#fractionAddition .resultDenominator').val(den);
-    }
 });
